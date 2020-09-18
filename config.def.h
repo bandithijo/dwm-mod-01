f4e6a0552502462ffe6ae67526a3c60244d242c6
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-09-18 13:34:13 +0800

    Add focusonclick config

--- config.def.h
+++ config.def.h
@@ -12,6 +12,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static int showsystray              = 0;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=8" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Medium:size=8";
 static char normbgcolor[]           = "#222222";
