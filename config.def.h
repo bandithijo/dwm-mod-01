26d87614eb043c61749772a02bd32cdf2caa0a17
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-10-03 23:40:16 +0800

    Change font to JetBrainsMono Nerd Font Bandit

--- config.def.h
+++ config.def.h
@@ -13,8 +13,8 @@ static int showsystray              = 0;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=8" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Medium:size=8";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Bandit:style=Medium:size=8" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Bandit:style=Medium:size=8";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
