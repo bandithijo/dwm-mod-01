d18eaa13fadaf0222d202298baeb0d415b4ebd0f
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-10-15 14:24:00 +0800

    Add bar height / 2 in manage()

--- dwm.c
+++ dwm.c
@@ -1070,7 +1070,7 @@ manage(Window w, XWindowAttributes *wa)
 	c->sfh = c->h;
 	if (c->iscentered) {
 		c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
-		c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
+		c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2 + (bh / 2);
 	}
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
