09c0a940123d39772ab8161eaef51d6a7ce86e53
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-11-16 14:33:59 +0800

    Change cursor position on center of the each monitor

--- dwm.c
+++ dwm.c
@@ -2055,7 +2055,8 @@ warp(const Client *c)
 	int x, y;
 
 	if (c || !c) {
-		XWarpPointer(dpy, None, root, 0, 0, 0, 0, selmon->wx + selmon->ww, selmon->wy + selmon->wh);
+        /* ww/2 & wh/2, for centering the cursor position */
+		XWarpPointer(dpy, None, root, 0, 0, 0, 0, selmon->wx + selmon->ww/2, selmon->wy + selmon->wh/2);
 		return;
 	}
 
