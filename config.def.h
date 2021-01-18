bfd36c4799df0f3531ec069291ad8817d5bbe6d3
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-01-18 17:51:25 +0800

    Add winconn window rule

--- config.def.h
+++ config.def.h
@@ -104,6 +104,7 @@ static const Rule rules[] = {
     { "Colorgrab",            NULL,                 NULL,                         0,             0,            1,           -1 },
     { "Pidgin",               NULL,                 NULL,                         0,             0,            1,           -1 },
     { "Pidgin",               NULL,                 "Buddy List",                 0,             0,            1,           -1 },
+    { "Winconn",              NULL,                 "WinConn",                    0,             1,            1,           -1 },
     /* class                  instance              title                         tags mask      iscentered    isfloating   monitor */
     /* Scratchpad */
     { NULL,                   NULL,                 "hidden",               scratchpad_mask,     0,            1,           -1 },
