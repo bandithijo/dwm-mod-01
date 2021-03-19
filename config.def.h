397a70480f776b7fee7bc02ebb4f19d8defa9657
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-03-19 22:10:47 +0800

    Add Polkit-mate-authentication-agent-1 window rule

--- config.def.h
+++ config.def.h
@@ -110,6 +110,7 @@ static const Rule rules[] = {
     { "Pidgin",               NULL,                 NULL,                         0,             0,            1,           -1 },
     { "Pidgin",               NULL,                 "Buddy List",                 0,             0,            1,           -1 },
     { "Wihotspot-gui",        NULL,                 "Wi Hotspot",                 0,             0,            1,           -1 },
+    { "Polkit-mate-authentication-agent-1", NULL,   "Authenticate",               0,             1,            1,           -1 },
     /* class                  instance              title                         tags mask      iscentered    isfloating   monitor */
     /* Scratchpad */
     { NULL,                   NULL,                 "hidden",               scratchpad_mask,     0,            1,           -1 },
