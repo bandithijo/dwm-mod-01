48868c15604fae1a0cf8134b3b9e1b8be628d7cf
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-05-19 00:03:02 +0800

    Enable togglebar keybind

--- config.def.h
+++ config.def.h
@@ -161,6 +161,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,             SHCMD("/usr/bin/dmenu-apps") },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,             {.v = termcmd } },
+    { MODKEY,                       XK_b,      togglebar,         {0} },
 	{ MODKEY,                       XK_j,      focusstack,        {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,        {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,        {.i = +1 } },
