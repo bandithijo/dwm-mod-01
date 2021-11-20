f9abfcf14b7ae7001640c1bf6956acc0b3ec6534
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-11-20 12:29:29 +0800

    Change gaps from 4 into 7

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int gappx              = 4;        /* gaps between windows */
+static const int gappx              = 7;        /* gaps between windows */
 static const unsigned int snap      = 5;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -211,8 +211,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,            {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,            {.i = +1 } },
 	{ MODKEY,                       XK_F5,     xrdb,              {.v = NULL } },
-	{ MODKEY,                       0x5b,      setgaps,           {.i = -4 } },
-	{ MODKEY,                       0x5d,      setgaps,           {.i = +4 } },
+	{ MODKEY,                       0x5b,      setgaps,           {.i = -1 } },
+	{ MODKEY,                       0x5d,      setgaps,           {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,           {.i = 0  } },
     { MODKEY|ControlMask,           XK_space,  focusmaster,       {0} },
 	{ MODKEY,                       XK_u,      swalstopsel,       {0} },
