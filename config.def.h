7bcec6ca338bbff537ba8b3ae1eee2db7cbcbd57
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-12-12 11:24:58 +0800

    Change default gap into 0

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int gappx              = 7;        /* gaps between windows */
+static const int gappx              = 0;        /* gaps between windows */
 static const unsigned int snap      = 5;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
