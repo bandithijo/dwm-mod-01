2558c5dcc20bdd5d67ab0e30bf0cf7458d6b86de
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-04-10 09:34:53 +0800

    Add gnome-calculator window rule

--- config.def.h
+++ config.def.h
@@ -86,6 +86,7 @@ static const Rule rules[] = {
     { "Gcolor3",              NULL,                 "Color Picker",               0,             0,            1,           -1 },
     { "Gcolor3",              NULL,                 "Color picker",               0,             0,            1,           -1 },
     { "Gnome-calculator",     NULL,                 "Calculator",                 0,             0,            1,           -1 },
+    { "gnome-calculator",     NULL,                 "Calculator",                 0,             0,            1,           -1 },
     { "Hexchat",              NULL,                 "Network List - HexChat",     1 << 5,        1,            1,           -1 },
     { "SimpleScreenRecorder", NULL,                 NULL,                         0,             1,            1,           -1 },
     { "Soffice",              NULL,                 "Print",                      0,             1,            1,           -1 },
