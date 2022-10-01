02431fc48cf102c23f8b17df1829a0b532cb077c
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-10-02 04:29:10 +0800

    Add topbarpadding variable

--- dwm.c
+++ dwm.c
@@ -699,7 +699,7 @@ dirtomon(int dir)
 void
 drawbar(Monitor *m)
 {
-	int x, w, tw = 0;
+	int x, w, tw = 0, bh = topbarpadding;
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
