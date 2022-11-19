65bd299d352eed761c1ae3f1a54fcbef33b8600c
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-11-19 15:42:41 +0800

    Remove dmenumon on spawn()

--- dwm.c
+++ dwm.c
@@ -1688,8 +1688,6 @@ sigchld(int unused)
 void
 spawn(const Arg *arg)
 {
-	if (arg->v == dmenucmd)
-		dmenumon[0] = '0' + selmon->num;
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
