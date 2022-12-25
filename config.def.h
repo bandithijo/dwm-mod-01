d0d4c9b51cbc5fb45430491f367245be52e32033
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-12-26 01:54:35 +0800

    Change gappx value

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int gappx              = 9;        /* gaps between windows */
+static const int gappx              = 14;       /* gaps between windows */
 static const unsigned int snap      = 5;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int user_bh            = 32;       /* bar height, default 18 */
@@ -233,8 +233,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,            {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,            {.i = +1 } },
 	{ MODKEY,                       XK_F5,     xrdb,              {.v = NULL } },
-	{ MODKEY,                       0x5b,      setgaps,           {.i = -9 } },
-	{ MODKEY,                       0x5d,      setgaps,           {.i = +9 } },
+	{ MODKEY|ShiftMask,             0x5b,      setgaps,           {.i = -14 } },
+	{ MODKEY|ShiftMask,             0x5d,      setgaps,           {.i = +14 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,           {.i = 0  } },
 	{ MODKEY|ControlMask,           XK_space,  focusmaster,       {0} },
 	{ MODKEY,                       XK_u,      swalstopsel,       {0} },
