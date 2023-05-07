95844bc38810cc59aed9e4201774ca5d3bb421d1
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2023-05-07 22:15:33 +0800

    Remove dmenumon

--- dwm.c
+++ dwm.c
@@ -1649,8 +1649,6 @@ spawn(const Arg *arg)
 {
 	struct sigaction sa;
 
-	if (arg->v == dmenucmd)
-		dmenumon[0] = '0' + selmon->num;
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
